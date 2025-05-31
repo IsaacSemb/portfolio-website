541206f6d45d828345ea9eb1f50b3b1509ed7337
IsaacSemb <isaacsemb1996@gmail.com>
2025-05-31 18:38:18 +0100

    Create the necessary Pages to route between

---
+++ src/pages/About.tsx
@@ -0,0 +1,9 @@
+
+function About() {
+    // this renders the about me page
+    return (
+    <div>About</div>
+  )
+}
+
+export default About
---
+++ src/pages/Contact.tsx
@@ -0,0 +1,12 @@
+
+
+function Contact() {
+    
+    // this renders the contact me page
+
+    return (
+    <div>Contact</div>
+  )
+}
+
+export default Contact
