60335426ede0c1eecb86b340da08466d1bf1c7ef
IsaacSemb <isaacsemb1996@gmail.com>
2025-05-31 19:02:02 +0100

    Remove NavBar from homePage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,11 +1,8 @@
-// import NavBar from "../components/NavBar"
-
 
 function HomePage() {
     // this renders the home page
     return (
     <div>
-        {/* <NavBar/> */}
         Home Page
     </div>
   )
